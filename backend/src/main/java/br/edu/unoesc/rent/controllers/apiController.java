e09d769410d6615dd6fc2261bad6f41ed26b3080
Danimar Varisa <danimarvarisa@gmail.com>
2022-05-03 04:54:18 -0300

    Ajuste de conflitos

--- backend/src/main/java/br/edu/unoesc/rent/controllers/apiController.java
+++ backend/src/main/java/br/edu/unoesc/rent/controllers/apiController.java
@@ -2,10 +2,6 @@ package br.edu.unoesc.rent.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-<<<<<<< HEAD
-=======
-import org.springframework.web.bind.annotation.RequestMapping;
->>>>>>> 02c77975d692d6960e72b23c6ac1020e7c1d45eb
 
 @Controller
 public class apiController {
